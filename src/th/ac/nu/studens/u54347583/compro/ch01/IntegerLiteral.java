6b70ee6999460cbefb96ede212c3c1fc9fbbd31f
Tuvanon <aam_ppshidae@msn.com>
2012-07-06 12:38:25 +0700

    commit
    IntegerLiteral

---
+++ src/th/ac/nu/studens/u54347583/compro/ch01/IntegerLiteral.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.studens.u54347583.compro.ch01;
+
+/**
+ *
+ * @author User
+ */
+public class IntegerLiteral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(12);
+        System.out.println(012);
+        System.out.println(0x12);
+        // TODO code application logic here
+    }
+}
